f2f4ca9a3fcd3a32115521294ca69f498422bafc
hubosin <jim617011@gmail.com>
2025-07-21 20:22:37 -0700

    importanta

--- src/main/java/gooder/modid/GooderMincraft.java
+++ src/main/java/gooder/modid/GooderMincraft.java
@@ -4,7 +4,7 @@ import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+//importanta
 public class GooderMincraft implements ModInitializer {
 	public static final String MOD_ID = "goodermincraft";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
